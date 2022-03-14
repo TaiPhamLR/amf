dca6cebed4ea1b3f8ecaf66a2fbe6f7b5a2e481a
Tai Pham <tai.pham@gs.liferay.com>
2022-03-14 22:56:27 +0700

    use model Liferay kernel User instead of new model Member

--- amf-web/src/main/java/com/liferay/training/amf/web/constants/MVCCommandNames.java
+++
@@ -1,8 +0,0 @@
-package com.liferay.training.amf.web.constants;
-
-public class MVCCommandNames {
-
-	public static final String EDIT_MEMBER = "/amf/member/edit";
-	public static final String ADD_MEMBER = "/amf/member/add";
-	public static final String VIEW_MEMBERS = "/amf/members/view";
-}
